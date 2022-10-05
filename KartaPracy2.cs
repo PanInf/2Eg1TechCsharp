045d9443007aaea437c281a5e5303dbd00fd50db
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-05 10:59:00 +0200

    Dodanie zadań 3-7 do KartaPracy2.cs

--- KartaPracy2.cs
+++ KartaPracy2.cs
@@ -37,6 +37,80 @@ namespace KartaPracy2
                 Console.WriteLine("NIE");
             }
             */
+            
+            // Zad 3
+            /*
+            Console.WriteLine("Ile masz lat?");
+            int wiek = int.Parse(Console.ReadLine());
+            if (wiek >= 18)
+            {
+                Console.WriteLine("TAK, jesteś pełnoletni");
+            }
+            else
+            {
+                Console.WriteLine("NIE, nie jesteś pełnoletni");
+            }
+            */
+            
+            // Zad 4
+            /*
+            const int LIMIT = 20;
+            int waga = int.Parse(Console.ReadLine());
+            if (LIMIT >= waga)
+            {
+                Console.WriteLine("TAK, możesz jechac");
+            }
+            else
+            {
+                Console.WriteLine("NIE, lepiej nie jedz");
+            }
+            */
+
+            // Zad 5
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if ((a < c) && (b > c) || (b < c) && (a > c))
+            {
+                Console.WriteLine("TAK, mieści się");
+            }
+            else
+            {
+                Console.WriteLine("NIE, nie mieści się");
+            }
+            */
+
+            // Zad 6
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            if ((Math.Pow(a, p) - a) % p == 0)
+            {
+                Console.WriteLine("TAK, spełnają MTF");
+            }
+            else
+            {
+                Console.WriteLine("NIE, nie spełniają MTF");
+            }
+            */
+
+            // Zad 7
+            /*
+            int p = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+
+            if ((p + 3*s) >= k)
+            {
+                Console.WriteLine("TAK, zaba da rade");
+            }
+            else
+            {
+                Console.WriteLine("NIE, nie da rady");
+            }
             Console.ReadKey();
         }
     }
