1178c1bf8ffa6fddf77e152b4ff588758ac021bc
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-05 10:44:19 +0200

    Wykonanie Zadań 1-2 z KartaPracy2.pdf

---
+++ KartaPracy2.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartaPracy2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zad 1
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a % 3 == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }
+            */
+
+            // Zad 2
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 100 && a < 1000 && a % 17 == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }
+            */
+            Console.ReadKey();
+        }
+    }
+}
