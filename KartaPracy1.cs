d9828d395380dcc2a32b472e588340f767c5f16a
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-05 10:09:21 +0200

    Wykonanie zadań KartaPracy1.pdf

---
+++ KartaPracy1.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartaPracy1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zad 1
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*a + b*b);
+            */
+
+            // Zad 2
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow((a + b),2));
+            */
+
+            // Zad 3
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow((a - b), 3));
+            */
+
+            // Zad 4
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * b * c);
+            */
+
+            // Zad 5
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(2*(a+b)/5);
+            */
+
+            // Zad 6
+            /*
+            double brutto = double.Parse(Console.ReadLine());
+            Console.WriteLine(brutto / 1.23);
+            */
+
+            // Zad 7
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a % b);
+            */
+
+            // Console.ReadKey();
+        }
+    }
+}
