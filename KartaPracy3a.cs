f6eca509d8d7a32c7c8d97f7cdd802d8f10f295e
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-26 10:48:36 +0200

    Update KartaPracy3a.cs

--- KartaPracy3a.cs
+++ KartaPracy3a.cs
@@ -1,70 +1,202 @@
 // Zad 1
-      // int n = int.Parse(Console.ReadLine());
-      // for (int i = 0; i < n; i++)
-      // {
-      //     Console.Write("*-|");
-      // }
+// int n = int.Parse(Console.ReadLine());
+// for (int i = 0; i < n; i++)
+// {
+//     Console.Write("*-|");
+// }
 
-      // Zad 2 - PRE
-      // Tabliczke mnozenia
-      // for (int i=1; i < 11; i++)
-      // {
-      //     for (int j=1 ; j < 11; j++)
-      //     {
-      //         Console.Write(i*j + "\t");
-      //     }
-      //     Console.WriteLine();
-      // }
+// Zad 2 - PRE
+// Tabliczke mnozenia
+// for (int i=1; i < 11; i++)
+// {
+//     for (int j=1 ; j < 11; j++)
+//     {
+//         Console.Write(i*j + "\t");
+//     }
+//     Console.WriteLine();
+// }
 
-      // *
-      // **
-      // ***
-      // ****
-      // *****
+// *
+// **
+// ***
+// ****
+// *****
 
-      // *****
-      // ****
-      // ***
-      // **
-      // *
+// *****
+// ****
+// ***
+// **
+// *
 
-      //     *
-      //    **
-      //   ***
-      //  ****
-      // *****
+//     *
+//    **
+//   ***
+//  ****
+// *****
 
-      int n = 5;
-      for (int i=0; i < n; i++)
-      {
-          for (int j=0 ; j < i+1; j++)
-          {
-              Console.Write("*" + "\t");
-          }
-          Console.WriteLine();
-      }
-      Console.WriteLine();
-      Console.WriteLine();
-      for (int i=0; i < n; i++)
-      {
-          for (int j=0 ; j < n-i; j++)
-          {
-              Console.Write("*" + "\t");
-          }
-          Console.WriteLine();
-      }
+// *****
+//  ****
+//   ***
+//    **
+//     *
 
-      Console.WriteLine();
-      Console.WriteLine();
-      for (int i=0; i < n; i++)
-      {
-          for (int j=0 ; j < n-i-1; j++)
-          {
-              Console.Write(" " + "\t");
-          }
-          for (int k=n-i-1 ; k < n; k++)
-          {
-              Console.Write("*" + "\t");
-          }
-          Console.WriteLine();
-      }
+/*
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < i + 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - i - 1; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = n - i - 1; k < n; k++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = i; k < n; k++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+*/
+/*
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < n + 1; i++)
+{
+    for (int j = 1; j < n + 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}*/
+
+/*
+
+*****
+*****
+*****
+*****
+*****
+
+punkty (i, j)
+
+(1, 1) (1, 2) (1, 3) (1, 4) (1, 5)
+(2, 1) (2, 2) (2, 3) (2, 4) (2, 5)
+(3, 1) (3, 2) (3, 3) (3, 4) (3, 5)
+(4, 1) (4, 2) (4, 3) (4, 4) (4, 5)
+(5, 1) (5, 2) (5, 3) (5, 4) (5, 5)
+* 
+**
+***
+****
+*****
+
+
+*/
+
+/*
+(1)
+// *
+// **
+// ***
+// ****
+// *****
+
+(2)
+// *****
+// ****
+// ***
+// **
+// *
+
+(3)
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+(4)
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+*/
+
+/*
+// 1
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i < n + 1; i++)
+{
+    for (int j = 1; j < n + 1; j++)
+    {
+        if (i >= j) Console.Write("*");
+        else        Console.Write(" ");    
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine();
+
+// 2
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i < n + 1; i++)
+{
+    for (int j = 1; j < n + 1; j++)
+    {
+        if (i >= j) Console.Write("*");
+        else Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine();
+
+
+// 4
+for (int i = 1; i < n + 1; i++)
+{
+    for (int j = 1; j < n + 1; j++)
+    {
+        if (j >= i) Console.Write("*");
+        else Console.Write(" ");
+    }
+    Console.WriteLine();
+}
